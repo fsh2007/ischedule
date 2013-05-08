b958ed0bbe6fe5b2f0b518274d4da36de9f7385f
jasonudoo <jasonudoo@gmail.com>
2013-05-08 15:33:53 +0800

    commit the front end javascript

--- wp-content/plugins/birchschedule/assets/js/birchschedule.js
+++ wp-content/plugins/birchschedule/assets/js/birchschedule.js
@@ -22,7 +22,7 @@ jQuery(function($){
     function changeServiceOptions() {
         var clientTypeId = $('#birs_appointment_client_type').val();
         $('#birs_appointment_service_div').html('');
-        var services = params.service_length_map[clientTypeId];
+        var services = params.service_map[clientTypeId];
         var htmlStr = "";
         var idv = "birs_appointment_service_";
         $.each(service, function(key, values){
